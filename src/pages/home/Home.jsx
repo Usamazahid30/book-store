58246946200e656c1272e8aba0565bbbaede67b2
Muhammad Usama/IT/HO <zahidusama20@gmail.com>
2024-12-24 16:56:15 +0500

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,16 @@
+import Banner from "./Banner";
+import News from "./News";
+import Recommened from "./Recommened";
+import TopSellers from "./TopSellers";
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <TopSellers />
+      <Recommened />
+      <News />
+    </div>
+  );
+};
+export default Home;
