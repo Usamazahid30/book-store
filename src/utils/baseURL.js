7786d1bb0bc64545370d8bcc7b5c4a4239e5de5b
Muhammad Usama/IT/HO <zahidusama20@gmail.com>
2025-01-21 10:10:09 +0500

    settingURLForProduc

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,7 +2,7 @@ const getBaseUrl = () => {
   if (window.location.hostname === "localhost") {
     return "http://localhost:5000"; // For local development
   } else {
-    return "https://book-store-backend-green-delta.vercel.app/"; // For production
+    return "https://book-store-backend-green-delta.vercel.app"; // For production
   }
 };
 
