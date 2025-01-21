9677aafc2d51eb02d7591721f0c1779e76b41a58
Muhammad Usama/IT/HO <zahidusama20@gmail.com>
2025-01-21 09:40:14 +0500

    settingURLForProduc

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,5 +1,9 @@
 const getBaseUrl = () => {
-  return "http://localhost:5000";
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:5000"; // For local development
+  } else {
+    return "https://book-store-backend-eta-eight.vercel.app/"; // For production
+  }
 };
 
 export default getBaseUrl;
